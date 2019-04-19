b9fd9fcfe7c10330c0486c65130fe4dd1076d88c
AndrewIvlev <mr.andrew.ivlev@gmail.com>
2019-04-19 22:19:58 +0300

    Added one green cell

--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -13,9 +13,10 @@ namespace thegame.Services
             {
                 new CellDto("1", new Vec(3, 4), "color1", "", 0),
                 new CellDto("2", new Vec(5, 4), "color1", "", 0),
-                new CellDto("3", new Vec(3, 1), "color1", "", 20),
-                new CellDto("4", new Vec(1, 0), "color2", "", 20),
-                new CellDto("5", movingObjectPosition, "color4", "☺", 10),
+                new CellDto("3", new Vec(5, 5), "color2", "", 20),
+                new CellDto("4", new Vec(3, 1), "color2", "", 20),
+                new CellDto("5", new Vec(1, 0), "color2", "", 20),
+                new CellDto("6", movingObjectPosition, "color4", "☺", 10),
             };
             return new GameDto(testCells, true, true, width, height, Guid.Empty, movingObjectPosition.X == 0, movingObjectPosition.Y);
         }
