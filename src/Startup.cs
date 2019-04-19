a224a808839c70e28ca027ad0c8e228c684773f3
vakym <avakym92@gmail.com>
2019-04-19 21:17:06 +0300

    test_commit

--- src/Startup.cs
+++ src/Startup.cs
@@ -19,6 +19,7 @@ namespace thegame
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
