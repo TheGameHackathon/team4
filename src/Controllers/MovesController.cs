0595a5753a99bf164cb0e9e3aa86ba2d67e2b2cf
vakym <avakym92@gmail.com>
2019-04-20 11:29:07 +0300

    gg

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -9,10 +9,18 @@ namespace thegame.Controllers
     [Route("api/games/{gameId}/moves")]
     public class MovesController : Controller
     {
+        private readonly GameManager games;
+
+        public  MovesController(GameManager games)
+        {
+            this.games = games;
+        }
         [HttpPost]
         public IActionResult Moves(Guid gameId, [FromBody]UserInputForMovesPost userInput)
         {
-            var game = TestData.AGameDto(1, userInput.ClickedPos ?? new Vec(1, 1));
+            var game = games.GetGameById(gameId);
+            var color = game.Cells.FirstOrDefault(c => c.Pos.Equals(userInput.ClickedPos)).Type;
+            game.Cells[0].Type = color;
             return new ObjectResult(game);
         }
     }
