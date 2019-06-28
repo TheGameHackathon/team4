a4fb7ea6e99becc34ba240d6808c99d87aa10681
MrTinic <mrtinic489@mail.ru>
2019-06-28 11:27:59 +0500

    DB and Entities interfaces added

---
+++ src/Entity/GameEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thegame.Entity
+{
+    public class GameEntity
+    {
+        public Guid Id { get; set; }
+        public CardEntity[,] Cards { get; set; }
+
+        public GameEntity(CardEntity[,] cards)
+        {
+            Id = new Guid();
+            Cards = cards;
+        }
+    }
+}
---
+++ src/Entity/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thegame.Entity
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public UserEntity(string name)
+        {
+            Id = new Guid();
+            Name = name;
+        }
+    }
+}
