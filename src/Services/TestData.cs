2de87c983afa624e4a27ba15ca8fc0a2243f1672
sochnev <dmitriy63176@gmail.com>
2021-06-29 11:21:52 +0500

    Create the field

--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -11,11 +11,29 @@ namespace thegame.Services
             var height = 8;
             var testCells = new[]
             {
-                new CellDto("1", new VectorDto(2, 4), "color1", "", 0),
-                new CellDto("2", new VectorDto(5, 4), "color1", "", 0),
-                new CellDto("3", new VectorDto(3, 1), "color2", "", 20),
-                new CellDto("4", new VectorDto(1, 0), "color2", "", 20),
-                new CellDto("5", movingObjectPosition, "color4", "☺", 10),
+                
+                
+                new CellDto("1", new VectorDto(0, 0), "wall", "", 20),
+                new CellDto("2", new VectorDto(1, 0), "wall", "", 20),
+                new CellDto("3", new VectorDto(2, 0), "wall", "", 20),
+                new CellDto("4", new VectorDto(3, 0), "wall", "", 20),
+                new CellDto("6", new VectorDto(4, 0), "wall", "", 20),
+                new CellDto("7", new VectorDto(4, 1), "wall", "", 20),
+                new CellDto("8", new VectorDto(4, 2), "wall", "", 20),
+                new CellDto("9", new VectorDto(4, 3), "wall", "", 20),
+                new CellDto("10", new VectorDto(4, 4), "wall", "", 20),
+                new CellDto("11", new VectorDto(0,4),  "wall", "", 20),
+                new CellDto("12", new VectorDto(1,4),  "wall", "", 20),
+                new CellDto("13", new VectorDto(2,4),  "wall", "", 20),
+                new CellDto("14", new VectorDto(3,4),  "wall", "", 20),
+                new CellDto("15", new VectorDto(4,4),  "wall", "", 20),
+                new CellDto("16", new VectorDto(0, 0), "wall", "", 20),
+                new CellDto("17", new VectorDto(0, 1), "wall", "", 20),
+                new CellDto("18", new VectorDto(0, 2), "wall", "", 20),
+                new CellDto("19", new VectorDto(0, 3), "wall", "", 20),
+                new CellDto("20", new VectorDto(0, 4), "wall", "", 20),
+                
+                new CellDto("5", movingObjectPosition, "player", "", 10),
             };
             return new GameDto(testCells, true, true, width, height, Guid.Empty, movingObjectPosition.X == 0, movingObjectPosition.Y);
         }
