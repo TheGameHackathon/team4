45e5c4c85cff415a9f6c22982ad3d295cab7bea9
ivanarray <i.maliamow@yandex.ru>
2022-04-16 11:28:05 +0500

    add dev branch

--- src/Program.cs
+++ src/Program.cs
@@ -12,6 +12,7 @@ namespace thegame
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
