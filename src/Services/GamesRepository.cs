e21245df013d3828f9b1a5b488a0a22d0c5dde91
ivanarray <i.maliamow@yandex.ru>
2022-04-16 13:08:02 +0500

    done GamesRepository.cs

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using thegame.Models;
 
 namespace thegame.Services
 {
-    public class GamesRepo
+    public static class GamesRepo
     {
+        private static IDictionary<Guid, GameDto> games = new ConcurrentDictionary<Guid, GameDto>();
+
+        public static GameDto GetGame(Guid id) => games[id];
+
+        public static void AddOrUpdate(Guid id, GameDto game) => games[id] = game;
     }
 }
