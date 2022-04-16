3ec85e5809bf2584e51bebd9263151325d01ec00
unknown <visitor@K1607N0080383.kontur>
2022-04-16 14:27:49 +0500

    Extensions

---
+++ src/Models/Extensions.cs
@@ -0,0 +1,40 @@
+﻿namespace thegame.Models
+{
+    public static class Extensions
+    {
+        public static CellDto[] GetRow(this CellDto[] cells, int rowIndex, int numOfColElements)
+        {
+            var numOfRowElements = cells.Length / numOfColElements;
+
+            var row = new CellDto[numOfRowElements];
+            for (int i = 0, j = rowIndex; i < numOfRowElements; i++, j += numOfColElements)
+                row[i] = cells[j];
+
+            return row;
+        }
+
+        public static void SetRow(this CellDto[] cells, CellDto[] row, int rowIndex, int numOfColElements)
+        {
+            var numOfRowElements = cells.Length / numOfColElements;
+
+            for (int i = 0, j = rowIndex; i < numOfRowElements; i++, j += numOfColElements)
+                cells[j] = row[i];
+        }
+
+        public static CellDto[] GetColumn(this CellDto[] cells, int colIndex, int numOfColElements)
+        {
+            var col = new CellDto[numOfColElements];
+            for (int i = 0, j = colIndex * numOfColElements; i < numOfColElements; i++, j++)
+                col[i] = cells[j];
+
+            return col;
+        }
+
+        public static void SetColumn(this CellDto[] cells, CellDto[] column, int colIndex)
+        {
+            var numOfColElements = column.Length;
+            for (int i = 0, j = colIndex * numOfColElements; i < numOfColElements; i++, j++)
+                cells[j] = column[i];
+        }
+    }
+}
