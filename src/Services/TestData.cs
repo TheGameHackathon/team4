b96df82bbfd358f07e53dccc3330bfb89cbe9461
Ivan Krasheninnikov <krash577388@mail.ru>
2022-07-04 11:16:09 +0500

    background fix

--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -11,7 +11,7 @@ namespace thegame.Services
             var height = 8;
             var testCells = new[]
             {
-                new CellDto("1", new VectorDto {X = 2, Y = 4}, "color1", "", 0),
+                new CellDto("1", new VectorDto {X = 2, Y = 4}, "wall", "", 0),
                 new CellDto("2", new VectorDto {X = 5, Y = 4}, "color1", "", 0),
                 new CellDto("3", new VectorDto {X = 3, Y = 1}, "color2", "", 20),
                 new CellDto("4", new VectorDto {X = 1, Y = 0}, "color2", "", 20),
