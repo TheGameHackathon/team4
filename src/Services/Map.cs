e007827e6447112e093e47f28b17276037ec3080
Vurado-t <mixplay20102002@gmail.com>
2022-07-04 16:07:20 +0500

    ok

--- src/Services/Map.cs
+++ src/Services/Map.cs
@@ -2,37 +2,38 @@
 using System.Collections.Generic;
 using thegame.Models.DTO;
 
-namespace thegame.Services;
-
-public class Map
+namespace thegame.Services
 {
-    public Map(GameDto game)
+    public class Map
     {
-        Boxes = new HashSet<VectorDto>();
-        Targets = new HashSet<VectorDto>();
-        Table = new CellDto[game.Width][];
-        for (int i = 0; i < game.Width; i++) 
-            Table[i] = new CellDto[game.Height];
-
-        foreach (var gameCell in game.Cells)
+        public Map(GameDto game)
         {
-            var pos = gameCell.Pos;
-            if (gameCell.Type is "wall" or "box")
-                Table[pos.X][pos.Y] = gameCell;
-            switch (gameCell.Type)
+            Boxes = new HashSet<VectorDto>();
+            Targets = new HashSet<VectorDto>();
+            Table = new CellDto[game.Width][];
+            for (int i = 0; i < game.Width; i++)
+                Table[i] = new CellDto[game.Height];
+
+            foreach (var gameCell in game.Cells)
             {
-                case "box":
-                    Boxes.Add(gameCell.Pos);
-                    break;
-                case "target":
-                    Targets.Add(gameCell.Pos);
-                    break;
+                var pos = gameCell.Pos;
+                if (gameCell.Type is "wall" or "box")
+                    Table[pos.X][pos.Y] = gameCell;
+                switch (gameCell.Type)
+                {
+                    case "box":
+                        Boxes.Add(gameCell.Pos);
+                        break;
+                    case "target":
+                        Targets.Add(gameCell.Pos);
+                        break;
+                }
             }
         }
-    }
 
-    public CellDto[][] Table { get; }
-    public HashSet<VectorDto> Boxes { get; }
-    public HashSet<VectorDto> Walls { get; }
-    public HashSet<VectorDto> Targets { get; }
+        public CellDto[][] Table { get; }
+        public HashSet<VectorDto> Boxes { get; }
+        public HashSet<VectorDto> Walls { get; }
+        public HashSet<VectorDto> Targets { get; }
+    }
 }
