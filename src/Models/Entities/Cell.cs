14407e2ec507064566cd0fc049311a4a92404595
valdmity <vlsur@outlook.com>
2022-07-04 11:34:33 +0500

    Cell namespace fix

--- src/Models/Entities/Cell.cs
+++ src/Models/Entities/Cell.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 
-namespace thegame.Models.Entities;
-
-public record Cell(Point Pos, string Type, int Id);
+namespace thegame.Models.Entities
+{
+    public record Cell(Point Pos, string Type, int Id);
+}
